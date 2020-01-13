1f2642afd246fa51317d02262c1460cdcea67909
Aryeh <aryeh@users.noreply.github.com>
2020-01-13 09:30:01 +0200

    Changed example to show static test method

---
+++ example/eventnotifier_example.dart
@@ -0,0 +1,6 @@
+import 'package:eventnotifier/eventnotifier.dart';
+
+void main() {
+  // See the EventNotifier test static method for an example of use.
+  EventNotifier.test();
+}
--- example/eventnotifier_example.md
+++
@@ -1,1 +0,0 @@
-See the EventNotifier test static method for an example of use.
